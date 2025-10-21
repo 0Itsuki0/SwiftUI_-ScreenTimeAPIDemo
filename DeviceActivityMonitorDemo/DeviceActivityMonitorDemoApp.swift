8038ce6f5f044dec3ba4e638459d294c48532638
itsuki <itsuki.enjyoy@gmail.com>
2025-10-21 12:31:28 +0900

    Initial Commit

---
+++ DeviceActivityMonitorDemo/DeviceActivityMonitorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceActivityMonitorDemoApp.swift
+//  DeviceActivityMonitorDemo
+//
+//  Created by Itsuki on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DeviceActivityMonitorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
